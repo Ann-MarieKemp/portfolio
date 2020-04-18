4e0ec79beafd67c6aebd96208eeb5ccbbff0777e
Ann-MarieKemp <aedalenb@gmail.com>
2020-04-18 08:41:52 -0700

    changed layout for baking weeks page

--- src/pages/BakingWeeks.js
+++ src/pages/BakingWeeks.js
@@ -12,6 +12,11 @@ const BakingWeeks = () => {
       <div className="main-page-container">
         <div className="baking-page">
           <p className="page-header">52 Weeks of Baking 2015</p>
+          <p className="description-text baking">
+            In 2015 Ann-Marie participated in a reddit challenge where she baked
+            a different dessert every week for a year. These are the posts she
+            made on reddit from that year.
+          </p>
           {posts.map(post => {
             return (
               <div className="weeks-of-baking-container">
