a0f012ad3320f7e0c1cc4a034cea3a74778b7e03
Ann-MarieKemp <aedalenb@gmail.com>
2020-04-17 17:10:45 -0700

    hooked up mdx for posts through gatsby-node

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,13 +1,27 @@
 import React from "react"
 import { graphql } from "gatsby"
 import "../styles/posts.css"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/Layout"
 
-const Post = props => {
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      id
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`
+const Post = ({ data: { mdx: post } }) => {
   return (
     <Layout>
-      <h1></h1>
-      <p>post by </p>
+      <h1>{post.frontmatter.title}</h1>
+      <p>post by {post.frontmatter.author} </p>
+      <MDXRenderer>{post.body}</MDXRenderer>
     </Layout>
   )
 }
