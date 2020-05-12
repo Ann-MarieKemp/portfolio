72f92fd59f34d5da2fe1983bca63740cba08e86d
Ann-MarieKemp <aedalenb@gmail.com>
2020-05-12 19:11:18 -0400

    added ogimage meta tag to helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./Navbar"
 import { Helmet } from "react-helmet"
 import useMetadata from "../hooks/useMetadata"
+import ogImage from "../../static/ogportfolio.jpg"
 
 const Layout = ({ children }) => {
   const { title, description } = useMetadata()
@@ -11,6 +12,8 @@ const Layout = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta property="og:url" content="https://a-mkemp.com" />
+        <meta property="og:image" content={ogImage} />
       </Helmet>
       <Navbar />
       <main>{children}</main>
