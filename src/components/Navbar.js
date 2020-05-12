f279522868989392d2f5d9f994b03a581bfa28a0
Ann-MarieKemp <aedalenb@gmail.com>
2020-05-12 13:24:24 -0400

    installed fontawesome packages and added icons for linked in and github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
 import "../styles/Navbar.css"
-import github from "../../static/GitHub-Mark-32px.png"
-import linkedin from "../../static/LI-In-Bug.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons"
+
 const Navbar = props => {
   return (
     <nav role="navigation" id="nav-bar-container">
-      <Link className="page-link" to="/">
-        Home
-      </Link>
-      <Link className="page-link" to="/Projects">
-        Projects
-      </Link>
-      <Link className="page-link" to="/AboutMe">
-        About
-      </Link>
-      <Link className="page-link" to="/Crafts">
-        Crafts
-      </Link>
-      <Link className="page-link" to="/ContactInfo">
-        Contact
-      </Link>
+      <div>
+        <Link className="page-link" to="/">
+          Home
+        </Link>
+        <Link className="page-link" to="/Projects">
+          Projects
+        </Link>
+        <Link className="page-link" to="/AboutMe">
+          About
+        </Link>
+        <Link className="page-link" to="/Crafts">
+          Crafts
+        </Link>
+        <Link className="page-link" to="/ContactInfo">
+          Contact
+        </Link>
+      </div>
+      <div className="icons-box">
+        <FontAwesomeIcon className="site-icons" icon={faGithub} />
+        <FontAwesomeIcon className="site-icons" icon={faLinkedin} />
+        <FontAwesomeIcon className="site-icons" icon={faFileDownload} />
+      </div>
       {/* <Link className="page-link" to="/Resume">
         Resume
       </Link> */}
